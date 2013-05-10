6e5ae5082088e52e5c68b809968f9464f71c7e2a
Mark Bayazit <mnbayazit@gmail.com>
2013-05-09 18:49:49 -0700

    string updates

--- Str.php
+++ Str.php
@@ -1,12 +1,14 @@
 <?php
 
 class Str {
-    public static function startsWith($haystack, $needle) {
-        return substr($haystack, 0, strlen($needle)) === $needle;
+    public static function startsWith($haystack, $needle, $case_sensitive = true) {
+        $substr = substr($haystack, 0, strlen($needle));
+        return $case_sensitive ? $substr === $needle : mb_strtolower($substr) === mb_strtolower($needle);
     }
 
-    public static function endsWith($haystack, $needle) {
-        return substr($haystack, -strlen($needle)) === $needle;
+    public static function endsWith($haystack, $needle, $case_sensitive = true) {
+        $substr = substr($haystack, -strlen($needle));
+        return $case_sensitive ? $substr === $needle : mb_strtolower($substr) === mb_strtolower($needle);
     }
 
     public static function phpTemplate($__file__, $__vars__) {
@@ -14,5 +16,9 @@ class Str {
         ob_start();
         include $__file__;
         return ob_get_clean();
+    }
+
+    public static function strSplit($str) {
+        return preg_split('//u', $str, -1, PREG_SPLIT_NO_EMPTY);
     }
 }

1bf8ac30a9e19cb7e8972fbd6edd4f1ceed32ef5
Mark Bayazit <mbayazit@nucleuslabs.com>
2014-05-13 16:46:34 -0700

    ds

--- autoload.php
+++ autoload.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'utilities' . DIRECTORY_SEPARATOR . 'FS.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'utilities' . DIRECTORY_SEPARATOR . 'Path.php';
 
 if(function_exists('__autoload')) {
     spl_autoload_register('__autoload');
@@ -7,12 +7,12 @@ if(function_exists('__autoload')) {
 
 set_include_path(implode(PATH_SEPARATOR, array(
     get_include_path(),
-    FS::joinPaths(__DIR__, 'callbacks'),
-    FS::joinPaths(__DIR__, 'collections'),
-    FS::joinPaths(__DIR__, 'dombuilder'),
-    FS::joinPaths(__DIR__, 'exceptions'),
-    FS::joinPaths(__DIR__, 'misc'),
-    FS::joinPaths(__DIR__, 'utilities'),
+    Path::join(__DIR__, 'callbacks'),
+    Path::join(__DIR__, 'collections'),
+    Path::join(__DIR__, 'dombuilder'),
+    Path::join(__DIR__, 'exceptions'),
+    Path::join(__DIR__, 'misc'),
+    Path::join(__DIR__, 'utilities'),
 )));
 
 spl_autoload_extensions('.php');

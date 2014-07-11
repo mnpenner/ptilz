01fe355596b108033cd59ff065ab8bffc93ac172
Mark Bayazit <mbayazit@nucleuslabs.com>
2014-07-11 09:10:23 -0700

    More array functions

--- src/Sql.php
+++ src/Sql.php
@@ -6,6 +6,7 @@ use mysqli;
 use PDO;
 use Ptilz\Exceptions\ArgumentTypeException;
 use Ptilz\Exceptions\UnreachableException;
+use Ptilz\Internal\RawSql;
 
 class Sql {
     /** @var PDO|mysqli Default connection used for escaping values  */
@@ -24,7 +25,7 @@ class Sql {
     public static function quote($value, $conn = null) {
         if(is_null($value)) return 'NULL';
         elseif(is_bool($value)) return $value ? '1' : '0';
-        elseif(is_int($value) || is_float($value) || $value instanceof _RawSql) return (string)$value;
+        elseif(is_int($value) || is_float($value) || $value instanceof RawSql) return (string)$value;
         elseif($value instanceof DateTime) return "'" . $value->format('Y-m-d H:i:s') . "'";
         elseif(is_array($value)) {
             if(Arr::isAssoc($value)) {
@@ -49,18 +50,18 @@ class Sql {
      * Returns a "literal" or "raw" value which will not be escaped by Sql::quote
      *
      * @param string $str Raw value (should be valid SQL)
-     * @return _RawSql
+     * @return RawSql
      */
     public static function raw($str) {
-        return new _RawSql($str);
+        return new RawSql($str);
     }
 
     public static function bin($str) {
-        return $str === null || $str === '' ? $str : new _RawSql('0x' . bin2hex($str));
+        return $str === null || $str === '' ? $str : new RawSql('0x' . bin2hex($str));
     }
 
     public static function escapeId($id, $forbidQualified = false) {
-        if($id instanceof _RawSql) return (string)$id;
+        if($id instanceof RawSql) return (string)$id;
         if(is_array($id)) {
             return implode(',', array_map(function ($x) use ($forbidQualified) {
                 return Sql::escapeId($x, $forbidQualified);

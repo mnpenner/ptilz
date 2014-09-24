cf566b096a333421d3ae2f65b8421e9ffa42e4c0
Mark Bayazit <mnbayazit@gmail.com>
2014-09-23 18:29:48 -0700

    Added colors

--- src/Static/Cli.php
+++ src/Static/Cli.php
@@ -8,11 +8,97 @@ use Ptilz\Exceptions\NotImplementedException;
 abstract class Cli {
 
     public static function write($format) {
-        echo call_user_func_array(['Str', 'format'], func_get_args());
+        echo self::colorize(call_user_func_array(['\Ptilz\Str', 'format'], func_get_args()));
     }
 
     public static function writeLine($format) {
-        echo call_user_func_array(['Str', 'format'], func_get_args()) . PHP_EOL;
+        echo self::colorize(call_user_func_array(['\Ptilz\Str', 'format'], func_get_args())) . PHP_EOL;
+    }
+
+    public static function colorize($str) {
+        return Str::replace([
+            '<b>' => "\033[1m",
+            '<bold>' => "\033[1m",
+            '<bright>' => "\033[1m",
+            '<strong>' => "\033[1m",
+            '</b>' => "\033[2m", // '21' should work, but it doesn't...?
+            '</bold>' => "\033[2m",
+            '</bright>' => "\033[2m",
+            '</strong>' => "\033[2m",
+
+            '<d>' => "\033[2m",
+            '<dim>' => "\033[2m",
+            '</d>' => "\033[22m",
+            '</dim>' => "\033[22m",
+
+            '<u>' => "\033[4m",
+            '<underline>' => "\033[4m",
+            '<underscore>' => "\033[4m",
+            '</u>' => "\033[24m",
+            '</underline>' => "\033[24m",
+            '</underscore>' => "\033[24m",
+
+            '<blink>' => "\033[5m",
+            '</blink>' => "\033[25m",
+
+            '<i>' => "\033[7m",
+            '<inverse>' => "\033[7m",
+            '<reverse>' => "\033[7m",
+            '</i>' => "\033[27m",
+            '</inverse>' => "\033[27m",
+            '</reverse>' => "\033[27m",
+
+            '<hidden>' => "\033[8m",
+            '</hidden>' => "\033[28m",
+            '<conceal>' => "\033[8m",
+            '</conceal>' => "\033[28m",
+
+            '<fg:black>' => "\033[30m",
+            '<fg:red>' => "\033[31m",
+            '<fg:green>' => "\033[32m",
+            '<fg:yellow>' => "\033[33m",
+            '<fg:blue>' => "\033[34m",
+            '<fg:magenta>' => "\033[35m",
+            '<fg:cyan>' => "\033[36m",
+            '<fg:lgray>' => "\033[37m",
+            '<fg:lgrey>' => "\033[37m",
+            '<fg:dgray>' => "\033[90m",
+            '<fg:dgrey>' => "\033[90m",
+            '<fg:lred>' => "\033[91m",
+            '<fg:lgreen>' => "\033[92m",
+            '<fg:lyellow>' => "\033[93m",
+            '<fg:lblue>' => "\033[94m",
+            '<fg:lmagenta>' => "\033[95m",
+            '<fg:lcyan>' => "\033[96m",
+            '<fg:white>' => "\033[97m",
+            '<fg:default>' => "\033[39m",
+            '</fg>' => "\033[39m",
+
+            '<reset>' => "\033[0m",
+            '<clear>' => "\033[0m",
+            '<default>' => "\033[0m",
+
+            '</bg>' => "\033[49m",
+            '<bg:default>' => "\033[49m",
+            '<bg:black>' => "\033[40m",
+            '<bg:red>' => "\033[41m",
+            '<bg:green>' => "\033[42m",
+            '<bg:yellow>' => "\033[43m",
+            '<bg:blue>' => "\033[44m",
+            '<bg:magenta>' => "\033[45m",
+            '<bg:cyan>' => "\033[46m",
+            '<bg:lgray>' => "\033[47m",
+            '<bg:lgrey>' => "\033[47m",
+            '<bg:dgray>' => "\033[100m",
+            '<bg:dgrey>' => "\033[100m",
+            '<bg:lred>' => "\033[101m",
+            '<bg:lgreen>' => "\033[102m",
+            '<bg:lyellow>' => "\033[103m",
+            '<bg:lblue>' => "\033[104m",
+            '<bg:lmagenta>' => "\033[105m",
+            '<bg:lcyan>' => "\033[106m",
+            '<bg:white>' => "\033[107m",
+        ],$str);
     }
 
     /**

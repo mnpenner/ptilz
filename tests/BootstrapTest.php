42c5472ec29e3a7d6ce2ee74ef71f472261641f8
Mark Penner <mpenner@nucleuslabs.com>
2016-01-29 13:31:19 -0800

    Clean up (sorry)

--- tests/BootstrapTest.php
+++ tests/BootstrapTest.php
@@ -1,5 +1,4 @@
 <?php
-use Ptilz\Iter;
 
 class BootstrapTest extends PHPUnit_Framework_TestCase {
     function testIntdiv() {
@@ -9,7 +8,6 @@ class BootstrapTest extends PHPUnit_Framework_TestCase {
         $this->assertSame(1,intdiv(-3,-2));
         $this->assertSame(1,intdiv(PHP_INT_MAX,PHP_INT_MAX));
         $this->assertSame(1,intdiv(-PHP_INT_MAX-1,-PHP_INT_MAX-1));
-//        $this->assertSame(0,intdiv(-PHP_INT_MAX-1,-1)); // Division of PHP_INT_MIN by -1 is not an integer
         $this->assertSame(-3,intdiv(-10,3));
     }
 

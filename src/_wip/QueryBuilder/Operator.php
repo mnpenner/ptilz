d1dd2b354c806aed097ea7b39077cfb8e890d81e
Mark Penner <mpenner@nucleuslabs.com>
2016-03-30 09:40:38 -0700

    Delete the _wip classes; most of them have been ported to other projects

--- src/_wip/QueryBuilder/Operator.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class Operator {
-    protected static $operators = ['AND','&&','=',':=','BINARY','&','~','|','^','DIV','/','<=>','=','>=','IS NOT NULL','IS NOT','IS NULL','IS','<<','<=','<','LIKE','-','%','MOD','!=','<>','NOT LIKE','NOT REGEXP','NOT','!','||','OR','+','REGEXP','>>','RLIKE','SOUNDS LIKE','*','XOR'];
-}
--- src/_wip/QueryBuilder/SortDir.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace QueryBuilder;
-
-class SortDir {
-    /** Ascending order */
-    const ASC = 'ASC';
-    /** Descending order */
-    const DESC = 'DESC';
-}

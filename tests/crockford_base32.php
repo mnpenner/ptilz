733c0f0c67f3471dd07b4c679abbf8d6efd19614
Mark Penner <marknpenner@gmail.com>
2016-04-25 23:08:04 -0700

    Food for thought

--- tests/crockford_base32.php
+++ tests/crockford_base32.php
@@ -1,4 +1,17 @@
 <?php
+/*
+Some things to consider:
+
+> OpenSSL was the culprit. More specifically, the use of openssl_random_pseudo_bytes() when using PHP in forked child processes, as is the case when using PHP with Apache or PHP-FPM. The processes were wrapping, so the children would produce the same random sequences as previous children with the same process IDs.
+
+https://benramsey.com/blog/2016/04/ramsey-uuid/
+https://github.com/ramsey/uuid/issues/117
+https://github.com/ramsey/uuid/issues/90
+https://github.com/paragonie/random_compat
+https://paragonie.com/blog/2015/07/how-safely-generate-random-strings-and-integers-in-php
+https://github.com/ircmaxell/RandomLib
+ */
+
 
 // https://en.wikipedia.org/wiki/Base32#Crockford.27s_Base32
 use Ptilz\Bin;
@@ -94,15 +107,15 @@ function crockford32_decode($data) {
  * - Only 3 more bytes more than base64
  * - Case-insensitive
  *
- * @param bool $raw_output
  * @return string
  * @see http://www.crockford.com/wrmg/base32.html
  */
-function uuid($raw_output=false) {
-    $bytes = openssl_random_pseudo_bytes(16);
-    $bytes[15] = chr(ord($bytes) & 0b11111000); // chop off 3 bits to make 125 bits total
-//    return $raw_output ? $bytes : crockford32_encode($bytes);
-    return $raw_output ? $bytes : substr(crockford32_encode($bytes),0,-1);
+function uuid() {
+    return implode('', array_map(function ($d) {
+        return '0123456789abcdefghjkmnpqrstvwxyz'[$d];
+    },array_map('bindec',array_slice(str_split(implode('', array_map(function ($s) {
+        return str_pad($s, 8, '0', STR_PAD_LEFT);
+    }, array_map('decbin', array_map('ord', str_split(openssl_random_pseudo_bytes(16)))))),5),0,-1))));
 }
 
 //for($i=0; $i<100000; ++$i) {
@@ -112,28 +125,63 @@ function uuid($raw_output=false) {
 //    }
 //}
 
-for($i=0; $i<1; ++$i) {
-    //$bytes = Bin::secureRandomBytes(mt_rand(1, 100));
-    //if(crockford32_decode(crockford32_encode($bytes)) !== $bytes) {
-    //    dump($bytes);
-    //    exit;
-    //}
-//    dump(crockford32_encode($bytes));
-//    dump(uuid());
-    $uuid = uuid();
-    $raw = crockford32_decode($uuid);
-//    dump(strlen($uuid));
-//    dump(strlen($raw)); // fixme: wtf?? where'd the last byte go?
-//    $uuid = openssl_random_pseudo_bytes(16);
-    dump('uuid   '.$uuid);
-    dump('base32 '.crockford32_encode($raw));
-    dump('base64 '.base64_encode($raw));
-    dump('base16 '.bin2hex($raw));
+//for($i=0; $i<1; ++$i) {
+//    //$bytes = Bin::secureRandomBytes(mt_rand(1, 100));
+//    //if(crockford32_decode(crockford32_encode($bytes)) !== $bytes) {
+//    //    dump($bytes);
+//    //    exit;
+//    //}
+////    dump(crockford32_encode($bytes));
+////    dump(uuid());
+//    $uuid = uuid();
+//    $raw = crockford32_decode($uuid);
+////    dump(strlen($uuid));
+////    dump(strlen($raw)); // fixme: wtf?? where'd the last byte go?
+////    $uuid = openssl_random_pseudo_bytes(16);
+//    dump('uuid   '.$uuid);
+//    dump('base32 '.crockford32_encode($raw));
+//    dump('base64 '.base64_encode($raw));
+//    dump('base16 '.bin2hex($raw));
+//}
+
+for($i=0; $i<10; ++$i) {
+    dump(uuid());
 }
 
+//echo implode(' ',array_map(function($x) { return str_pad($x,5,'0',STR_PAD_RIGHT); },str_split(implode('', array_map(function ($s) {
+//        return str_pad($s, 8, '0', STR_PAD_LEFT);
+//    }, array_map('decbin', array_map('ord', str_split('f0oBaR'))))),5))).PHP_EOL;
+//
+//
+//echo implode(', ',array_map('bindec',array_map(function($x) { return str_pad($x,5,'0',STR_PAD_RIGHT); },str_split(implode('', array_map(function ($s) {
+//    return str_pad($s, 8, '0', STR_PAD_LEFT);
+//}, array_map('decbin', array_map('ord', str_split('f0oBaR'))))),5))));
 
 //dump(crockford32_encode('Mark'));
 //dump(crockford32_decode('9ngq4tr'));
+
+function f2($b, $n) {
+    return array_map('bindec', array_map(function ($x) use ($n) {
+        return str_pad($x, $n, '0', STR_PAD_RIGHT);
+    }, str_split(implode('', array_map(function ($s) {
+        return str_pad($s, 8, '0', STR_PAD_LEFT);
+    }, array_map('decbin', array_map('ord', str_split($b))))), 5)));
+}
+
+
+//function f($b,$n){return array_map('bindec',array_map(function($x)use($n){return str_pad($x,$n,'0');},str_split(implode('',array_map(function($s){return str_pad($s,8,'0',STR_PAD_LEFT);},array_map('decbin',$b))),5)));}
+function f($b, $n) {
+    return array_map('bindec', array_map(function ($x) use ($n) {
+        return str_pad($x, $n, '0');
+    }, str_split(implode('', array_map(function ($s) {
+        return str_pad($s, 8, '0', STR_PAD_LEFT);
+    }, array_map('decbin', $b))), 5)));
+}
+
+
+//echo implode(',',array_map('ord', str_split('f0oBaR')));
+dump(f2('f0oBaR',5));
+dump(f([102,48,111,66,97,82],5));
 
 
 //dump(crockford32_encode("A"));
